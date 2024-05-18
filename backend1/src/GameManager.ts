515a437560b8268a8a9b7ce783328b86daa7d72c
Tanishka <tanishkadeep09@gmail.com>
2024-05-18 17:54:21 +0530

    game manager logic

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,17 +1,35 @@
 import { WebSocket } from "ws";
+import { INIT_GAME } from "./messgae";
 
 export class GameManager {
   private games: Game[];
+  private pendingUser: WebSocket;
+  private users: WebSocket[];
 
   constructor() {
     this.games = [];
   }
 
-  addUser(socket: WebSocket) {}
+  addUser(socket: WebSocket) {
+    this.users.push(socket);
+    this.addHandler(socket);
+  }
 
-  removeUser(socket: WebSocket) {}
+  removeUser(socket: WebSocket) {
+    this.users = this.users.filter((user) => user != socket);
+  }
 
-  private handleMessage(){
+  private addHandler(socket: WebSocket) {
+    socket.on("message", (data) => {
+      const message = JSON.parse(data.toString());
 
+      if (message.type === INIT_GAME) {
+        if (this.pendingUser) {
+          //start a game
+        } else {
+          this.pendingUser = socket;
+        }
+      }
+    });
   }
 }
